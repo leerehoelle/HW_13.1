a3c62a2ce6c08f95bf232f9614ab4f3298aaf908
mariiasaliutina <m.salyutina@gmail.com>
2024-04-15 15:51:58 +0200

    tests are created

---
+++ tests.py
@@ -0,0 +1,26 @@
+import pytest
+from classes import Category, Product
+
+@pytest.fixture
+def sample_product():
+    return [Product("Мышь APPLE Magic Mouse", "Apple Magic Mouse 3 — многофункциональная мышь", 9128, 100),
+            Product("Смартфон Apple iPhone 15 256 ГБ", "Встречайте iPhone 15", 90366, 50)]
+
+
+@pytest.fixture
+def sample_category(sample_product):
+    return Category("Электроника", "Электроника компании Apple", sample_product)
+
+def test_category_initialization(sample_category, sample_product):
+    assert sample_category.name == "Электроника"
+    assert sample_category.description == "Электроника компании Apple"
+    assert sample_category.goods == sample_product
+    assert sample_product[0].name == "Мышь APPLE Magic Mouse"
+    assert sample_product[0].description == "Apple Magic Mouse 3 — многофункциональная мышь"
+    assert sample_product[0].price == 9128
+    assert sample_product[0].quantity == 100
+
+def test_product(sample_category, sample_product):
+    assert Category.unique_products == 2
+    assert Category.total_number == 4
+
