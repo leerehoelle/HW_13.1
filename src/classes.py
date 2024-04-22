49bc6e342bd4e23fe8f1c9d2e8e404625cfe8d60
Mariia Saliutina <114161012+leerehoelle@users.noreply.github.com>
2024-04-22 15:42:53 +0200

    15.1 commit back

--- src/classes.py
+++ src/classes.py
@@ -15,6 +15,8 @@ class Category:
 
 
     def goods_addition(self, product):
+        if not isinstance(self, product):
+            raise ValueError('Мы не можем добавлять то, что не является экземпляром класса Product')
         self.__goods.append(product)
         Category.unique_products += 1
 
